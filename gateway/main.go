eb39cfdf81b7a9a3198461bd4051ee06d53c245e
Md Aolad Hossain Anna <aolad.anna@gmail.com>
2026-04-13 11:24:38 +0200

    chng

--- gateway/main.go
+++ gateway/main.go
@@ -6,7 +6,6 @@ import (
 	"net/http/httputil"
 	"net/url"
 	"os"
-	"sports-stream-backend/pkg/util"
 	"strings"
 )
 
@@ -93,11 +92,16 @@ func main() {
 		port = "8080"
 	}
 
-	// NEW — read from environment (works on Cloud Run)
-	userProxy := newProxy(util.MustGetenv("USER_SERVICE_URL"))
-	streamProxy := newProxy(util.MustGetenv("STREAM_SERVICE_URL"))
-	analyticsProxy := newProxy(util.MustGetenv("ANALYTICS_SERVICE_URL"))
-	notificationProxy := newProxy(util.MustGetenv("NOTIFICATION_SERVICE_URL"))
+	//// NEW — read from environment (works on Cloud Run)
+	//userProxy := newProxy(util.MustGetenv("USER_SERVICE_URL"))
+	//streamProxy := newProxy(util.MustGetenv("STREAM_SERVICE_URL"))
+	//analyticsProxy := newProxy(util.MustGetenv("ANALYTICS_SERVICE_URL"))
+	//notificationProxy := newProxy(util.MustGetenv("NOTIFICATION_SERVICE_URL"))
+
+	userProxy := newProxy("http://127.0.0.1:8081")
+	streamProxy := newProxy("http://127.0.0.1:8082")
+	notificationProxy := newProxy("http://127.0.0.1:8083")
+	analyticsProxy := newProxy("http://127.0.0.1:8085")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		path := r.URL.Path
